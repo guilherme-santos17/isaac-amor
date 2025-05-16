73fd767b3d513cab92be5832297d643973e0bce2
Guilherme <113263504+guilherme-santos17@users.noreply.github.com>
2025-05-16 12:20:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function gerarQRCode() {
-    const urlDestino = " https://guilherme-santos17.github.io/isaac-amor/";
+    const urlDestino = "https://guilherme-santos17.github.io/isaac-amor/mensagem.html";
     const img = document.getElementById("qrcode");
   
     QRCode.toDataURL(urlDestino, { width: 200 }, function (err, dataUrl) {
