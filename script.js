a67843f1521e38d22b9476552e50c0cb118a176e
guilherme-santos17 <guilherme.fxaviersantos@gmail.com>
2025-05-29 16:44:56 -0300

    Mudança no QR Code

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-function gerarQRCode() {
-    const urlDestino = "https://guilherme-santos17.github.io/isaac-amor/mensagem.html";
+const urlDestino = "https://guilherme-santos17.github.io/isaac-amor/mensagem.html";
     const img = document.getElementById("qrcode");
-  
-    QRCode.toDataURL(urlDestino, { width: 200 }, function (err, dataUrl) {
+
+    QRCode.toDataURL(urlDestino, { width: 140 }, function (err, dataUrl) {
       if (err) {
         console.error(err);
         return;
       }
       img.src = dataUrl;
     });
-  }
-  
